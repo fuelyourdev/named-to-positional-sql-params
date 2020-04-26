37e99faa0818b1cea8ff4aab8cb698bd015f7fc8
Trevor Young <trevor@fuelyour.dev>
2020-04-26 01:11:46 -0600

    Release 0.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "dev.fuelyour"
-version = "0.0.4-SNAPSHOT"
+version = "0.0.4"
 val projectDescription = "Simple library that converts sql queries using " +
     "named parameters into sql queries using positional parameters, thus " +
     "allowing you to use named parameters even if your database tools only " +
