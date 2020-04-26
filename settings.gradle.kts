c66bb59ce4aa90f0cbf76278527ee2bad8a9ae7b
Trevor Young <trevor@fuelyour.dev>
2020-04-26 00:43:38 -0600

    Modified pipeline to allow for publishing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,1 @@
 rootProject.name = "named-to-positional-sql-params"
-
-val ossrhUsername: String? by settings
-val ossrhPassword: String? by settings
-extra["ossrhUsername"] = ossrhUsername ?: ""
-extra["ossrhPassword"] = ossrhPassword ?: ""
